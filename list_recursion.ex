28c3ebab415ed6fffec47d89c5f2fed533706c5c
Eden <untouchable.cloud@gmail.com>
2025-02-27 00:31:06 +0700

    done chapter 7 exercises

--- list_recursion.ex
+++ list_recursion.ex
@@ -17,7 +17,34 @@ defmodule MyList do
   def sum([head | tail], value), do: sum(tail, value + head)
 
   @doc """
+  Reduces a list to a single value using an accumulator and a function.
+  """
+  def reduce([], value, _func), do: value
+  def reduce([head | tail], value, func), do: reduce(tail, func.(head, value), func)
+
+  @doc """
   Applies a function to each element of the list and then sums the results.
   """
   def mapsum(list, func), do: list |> map(func) |> sum()
+
+  @doc """
+  Finds the maximum value in a list.
+  """
+  def max([head | tail]) do
+    reduce(tail, head, fn x, acc -> if x > acc, do: x, else: acc end)
+  end
+
+  def caesar([], _n), do: ""
+
+  def caesar(charlist, n) do
+    fun = fn char, n ->
+      cond do
+        char + n > ?z -> ?a + (char + n - ?z)
+        true -> char + n
+      end
+    end
+
+    charlist
+    |> Enum.map(&fun.(&1, n))
+  end
 end
